797926c8f2ef7dbf8a776a989cafa30ae2ba4da1
mir3x <mlewczuk80@gmail.com>
2020-10-01 12:46:34 +0200

    resizing works mostly

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# freeciv_tileset_modifier
+## freeciv_tileset_modifier
  Modifies tileset and writes back
+
+Resizes tileset (now doesnt resize included files yet)
+ Example scale tileset 3 times:
+
+ *python.exe fc_tileset.py trident.tilespec -output_file trident_3X -scale 300*
+
+
--- fc_tileset.py
+++ fc_tileset.py
@@ -31,6 +31,12 @@ tiles_list = list()
 sizes = [0, 0, 0, 0, 0]  # x,y,dx, dy, pb
 scale = 100
 
+def remove_comments(line):
+    if ";" in line:
+        x = line.find(";")
+        line = line[0:x] + "\n"
+    return line
+
 def load_dir(dir_name):
     global tiles_list
     files = []
@@ -157,15 +163,19 @@ def load_spec(filename, output_dir):
     #     print(f"Error ***{err}*** when reading file {filename}. Exiting")
     #     exit(1)
 
+def sc_v(val):
+    global scale
+    return int(val * scale /100)
 
 def load_image(filename, output_dir):
-
+    global scale
     box = tuple()
     filename = filename.replace("\"", "") + ".png"
     im = Image.open(filename)
 
     imsize = im.size
-    im_mod = Image.new("RGBA", imsize, "black")
+    newsize = (sc_v(im.size[0]), sc_v(im.size[1]))
+    im_mod = Image.new("RGBA", newsize, "black")
 
     for tile in tiles_list:
         # Could u see this this coming ?
@@ -175,7 +185,17 @@ def load_image(filename, output_dir):
                tile[Y] + tile[DY] * tile[SX] + tile[DY] + tile[PB] * tile[SX])
 
         region = im.crop(box)
-        im_mod.paste(region, box)
+        new_region = region.resize((sc_v(region.size[0]), sc_v(region.size[1])), Image.BILINEAR)
+        tile[X] = sc_v(tile[X])
+        tile[Y] = sc_v(tile[Y])
+        tile[DY] = sc_v(tile[DY])
+        tile[DX] = sc_v(tile[DX])
+        tile[PB] = sc_v(tile[PB])
+        newbox = (tile[X] + tile[DX] * tile[SY] + tile[PB] * tile[SY],
+                  tile[Y] + tile[DY] * tile[SX] + tile[PB] * tile[SX],
+                  tile[X] + tile[DX] * tile[SY] + tile[DX] + tile[PB] * tile[SY],
+                  tile[Y] + tile[DY] * tile[SX] + tile[DY] + tile[PB] * tile[SX])
+        im_mod.paste(new_region, newbox)
 
     bad_deadpool, good_deadpool = os.path.split(filename)
     print(f"Writing:{output_dir}{os.path.sep}{good_deadpool}")
@@ -186,8 +206,39 @@ def load_tileset(input_file):
     (eiskalt, erwischt) = os.path.splitext(input_file)
     load_dir(eiskalt)
 
+def replace_line_with_scale(line):
+    global scale
+    line = remove_comments(line)
+    newline = line
+    something = ["normal_tile_width", "normal_tile_height",
+                 "small_tile_width", "small_tile_height"]
+
+    for anything in something:
+        if anything in line:
+            left, right = line.split('=')
+            tostr_toint_scaledright = str(sc_v(int(right)))
+            newline = " ".join([left, "=", tostr_toint_scaledright, "\n"])
+
+    return newline
+
+# its the same JUST CAUSE
+def replace_line_grid_scale(line):
+    global scale
+    line = remove_comments(line)
+    newline = line
+    something = ["x_top_left", "y_top_left", "pixel_border",
+                 "dx", "dy", ]
+
+    for anything in something:
+        if anything in line:
+            left, right = line.split('=')
+            tostr_toint_scaledright = str(sc_v(int(right)))
+            newline = " ".join([left, "=", tostr_toint_scaledright, "\n"])
+
+    return newline
 
 def write_tilespec(filew, inputr):
+    global scale
     output_specfile = filew + ".tilespec"
     inr = inputr + ".tilespec"
     fh, abs_path = mkstemp()
@@ -205,6 +256,11 @@ def write_tilespec(filew, inputr):
                     newline = line[:a1] + "\"" + filew + line[a2:]
                     new_file.write(newline)
                     continue
+
+                if (scale != 100):
+                    newline = replace_line_with_scale(line)
+                    new_file.write(newline)
+                    continue
                 new_file.write(line)
     # Remove original file
     if os.path.isfile(output_specfile):
@@ -216,8 +272,9 @@ def write_tilespec(filew, inputr):
 
 def write_directory(output_file, input):
     global tiles_list
+    global scale
     files = []
-
+    where = "notgrid"
     if os.path.isdir(output_file):
         shutil.rmtree(output_file)
     os.mkdir(output_file)
@@ -241,7 +298,20 @@ def write_directory(output_file, input):
                                 output_file + line[a2:]
                             new_file.write(newline)
                             continue
+                        if line[0] == "[":
+                            if "[grid_" in line:
+                                where = "grid"
+                            else:
+                                where = "notgrid"
+
+                        if where == "grid" and scale !=100:
+                            newline = replace_line_grid_scale(line)
+                            new_file.write(newline)
+                            continue
+
                         new_file.write(line)
+
+
 
             # Remove original file
             if os.path.isfile(output_specfile):
@@ -271,7 +341,7 @@ if __name__ == '__main__':
     parser.add_argument('-output_file', type=str, metavar='string', nargs='?', default="XXX",
                         help='New tileset name (default: ($output_file), "\
                              "directory with that name will be wiped, you have been warned')
-    parser.add_argument('-scale', type=int, metavar='scale percent', nargs='?', default="200",
-                        help='New scale in percent (default: ($scale %) ')
+    parser.add_argument('-scale', type=int, metavar='percent', nargs='?', default="200",
+                        help='New scale in percent (default: ($scale) ')
     args = parser.parse_args()
     main(args.input_tilespec, args.output_file, args.scale)
